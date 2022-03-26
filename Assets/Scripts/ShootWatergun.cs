0f133cb223017a66ce3e7b6d9a212c4c090b6496
tecktim <67341872+tecktim@users.noreply.github.com>
2022-03-26 15:46:46 +0100

    homo

--- Assets/Scripts/ShootWatergun.cs
+++ Assets/Scripts/ShootWatergun.cs
@@ -5,33 +5,46 @@ using UnityEngine;
 public class ShootWatergun : MonoBehaviour
 {
     private CircleCollider2D waterGunCollider;
-    public GameObject waterGun;
+    public Transform waterGun;
+    public GameObject waterPrefab;
     private Vector3 worldMousePosition;
 
     // Start is called before the first frame update
     void Start()
     {
         waterGunCollider = GetComponent<CircleCollider2D>();
-        waterGun.transform.position = new Vector3(0f, 0f, -1f);
+        waterGun.position = new Vector3(0f, 0f, -1f);
     }
 
     // Update is called once per frame
     void Update()
     {
         worldMousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
-            waterGun.transform.position = new Vector3(worldMousePosition.x, worldMousePosition.y, 1f);
-            if(waterGun.transform.localScale.x < .5 && waterGun.transform.localScale.y < .5)
+            waterGun.gameObject.SetActive(true);
+            waterGun.localScale = new Vector3(.1f, .1f, 0);
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            waterGun.position = new Vector3(worldMousePosition.x, worldMousePosition.y, 1f);
+            if(waterGun.localScale.x < .5 && waterGun.localScale.y < .5)
             {
-                waterGun.transform.localScale += new Vector3(.001f, .001f, 0);
+                waterGun.localScale += new Vector3(.1f, .1f, 0)*Time.deltaTime;
             }
         }
-        else
+        else if(Input.GetMouseButtonUp(0))
         {
-            waterGun.transform.position = new Vector3(0f, 0f, -1f);
-            waterGun.transform.localScale = new Vector3(.1f, .1f, 0);
-            
+            GameObject waterInstance = Instantiate(waterPrefab, waterGun.position, waterGun.rotation);
+            waterInstance.transform.localScale = waterGun.localScale;
+            Destroy(waterInstance, 5f);
+            waterGun.gameObject.SetActive(false);
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
 }
